770cf07cef6f0b960d5d02fb2950e0ec97abe279
Giacomo Gorga <gorga@feedbackitalia.it>
2020-12-03 11:19:52 +0100

    Fix

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -15,7 +15,7 @@ function Header() {
                         <img src={mainLogo} width="30" height="30" className="d-inline-block align-top" alt="logo-image"/>PULSeBS Team 5
                     </a>
                     {context.authUser && (/*context.authUser.Type === "student" ||*/ context.authUser.Type === "teacher") && <Link to="/lectures"><Button variant="primary">Lectures</Button> </Link>}
-                    {context.authUser && (context.authUser.Type === "manager" || context.authUser.Type === "teacher") && <Link to="/courses"><Button variant="primary">Courses</Button></Link>}
+                    {context.authUser && (context.authUser.Type === "booking-manager" || context.authUser.Type === "teacher") && <Link to="/courses"><Button variant="primary">Courses</Button></Link>}
                     {context.authUser && <Link to="/login" onClick={context.logoutUser}><Button variant="danger">Logout</Button></Link>}
                 </nav>
                 {context.authUser && <div><br/><h1>Welcome {context.authUser.Name} {context.authUser.Surname}</h1><br/></div>}
